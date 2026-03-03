8fec9e72037ca49d4051da5a3bcdf9b8bdc00dfb
xinyi <xinyi.wanwan@gmail.com>
2026-03-03 13:06:51 +0100

    changed default pathname

--- dcm2nifti.py
+++ dcm2nifti.py
@@ -29,8 +29,8 @@ from pathlib import Path
 logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
 logger = logging.getLogger(__name__)
 
-DATADIR = Path(".")
-OUTDIR = Path("niftis")
+DATADIR = Path("/data")
+OUTDIR = Path("/results")
 
 
 # ---------------------------------------------------------------------------
