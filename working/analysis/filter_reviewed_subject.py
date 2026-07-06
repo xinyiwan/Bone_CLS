dbc39e6548331a29f5122f439b579685736ff321
xinyi <xinyi.wanwan@gmail.com>
2026-07-06 16:53:18 +0200

    script idea to filter out included and checked subjects

---
+++ working/analysis/filter_reviewed_subject.py
@@ -0,0 +1,6 @@
+import pandas as pd 
+
+### input combined_reviewed.csv which combined the reviewed subject from batch 1 and 2. colmn Paciente
+### Use this information to filter out the subject within batch_3/Results/Sequence_Classifier.csv and batch 4
+
+out put the filtered number, and saved the newsubject in another csv file call /Sequence_Classifier_filttered.csv
